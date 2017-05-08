43d80e401cc68ec1d4c544f6de94ad2ff15fa5e8
Иосиф Дзеранов <iosifdzeranov@Iosifs-MacBook-Air.local>
2017-05-08 11:36:49 +0300

    Initial Project

---
+++ Delegation-Design-Pattern/ColorViewDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ColorViewDelegate.swift
+//  Delegation-Design-Pattern
+//
+//  Created by Иосиф Дзеранов on 08/05/2017.
+//  Copyright © 2017 Иосиф Дзеранов. All rights reserved.
+//
+
+import Foundation
+protocol ColorViewDelegate {
+}
